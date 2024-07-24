8bc09a43602538cfae90571b0f467c723b9fb6d5
Dymtro Chesnokov <dimmando@ukr.net>
2024-07-24 17:40:40 +0000

    Create Welcome message and describe how program works

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from colorama import Fore, Back, Style  # import colors sheme
+
+def main():
+    """
+    Main function that starts the program that helps 
+    to find out your risk of type 2 diabetes in the next 10 years
+    """
+    print(Fore.YELLOW + "\nWelcome to our program that helps to find out your risk of type 2 diabetes in the next 10 years.")
+    print("To calculate your risk we will ask you for some data related to your health.") 
+    print("This program doesn't save or transfer any data you provided so anyone cannot identify you.\n" + Style.RESET_ALL)
+
+    print("When you pass through our test you'll find out that potential risk level for developing diabetes in the next 10 years of life is (depending on points earned):\n")
+    
+    print(Fore.GREEN + "Less than 7 points" + Style.RESET_ALL + ": Low risk")
+    print(Fore.YELLOW + "7 - 11 points" + Style.RESET_ALL + ": Slight risk")
+    print(Fore.CYAN + "12 - 14 points" + Style.RESET_ALL + ": Average risk")
+    print(Fore.MAGENTA + "15 - 20 points" + Style.RESET_ALL + ": High risk")
+    print(Fore.RED + "More than 20 points" + Style.RESET_ALL + ": Very high risk\n")
+    
+    print("Let's get started?\n")
+
+    
+
+main()
