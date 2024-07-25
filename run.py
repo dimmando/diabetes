9e1d9b3ba2107349e63605bdb049b50e30e8c01c
Dymtro Chesnokov <dimmando@ukr.net>
2024-07-25 10:22:53 +0000

    Optimizing code for 79 cols

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from colorama import Fore, Back, Style   # import colors sheme
+from colorama import Fore, Back, Style  # import colors sheme
 from simple_term_menu import TerminalMenu  # import Simple Terminal Menu
 import sys  # import sys library for correct exit from program
 from os import system  # import system functions for cleaning terminal
@@ -19,29 +19,47 @@ def main():
 
 def introduction():
     """Introduction function"""
-    print(f"""{Fore.YELLOW}
+    print(
+        f"""{Fore.YELLOW}
           Welcome to our program that helps to find out your risk of 
-                    type 2 diabetes in the next 10 years""")
-    print(f"""
+                    type 2 diabetes in the next 10 years"""
+    )
+    print(
+        f"""
   To calculate your risk we will ask you for some data related to your health.
   This program doesn't save or transfer any data you provided so anyone cannot 
-  identify you. You are in safe place.{Style.RESET_ALL}""")
-    print(f"""
+  identify you. You are in safe place.{Style.RESET_ALL}"""
+    )
+    print(
+        f"""
   When you pass through our test you'll find out that potential risk level for 
   developing diabetes in the next 10 years of life depending on points earned:
-    """)    
+    """
+    )
     print(Fore.GREEN + "Less than 7 points" + Style.RESET_ALL + ":  Low risk")
-    print(Fore.YELLOW + "7 - 11 points" + Style.RESET_ALL + ":       Slight risk")
-    print(Fore.CYAN + "12 - 14 points" + Style.RESET_ALL + ":      Average risk")
-    print(Fore.MAGENTA + "15 - 20 points" + Style.RESET_ALL + ":      High risk")
-    print(Fore.RED + "More than 20 points" + Style.RESET_ALL + ": Very high risk\n")
+    print(
+        Fore.YELLOW + "7 - 11 points" + Style.RESET_ALL + ":       Slight risk"
+    )
+    print(
+        Fore.CYAN + "12 - 14 points" + Style.RESET_ALL + ":      Average risk"
+    )
+    print(
+        Fore.MAGENTA + "15 - 20 points" + Style.RESET_ALL + ":      High risk"
+    )
+    print(
+        Fore.RED
+        + "More than 20 points"
+        + Style.RESET_ALL
+        + ": Very high risk\n"
+    )
 
 
 def menu_start():
     while True:
         try:
-            start = input("Let's get started? " + Fore.BLUE + "(Y/N): " +
-                    Style.RESET_ALL).upper()
+            start = input(
+                "Let's get started? " + Fore.BLUE + "(Y/N): " + Style.RESET_ALL
+            ).upper()
             if start == "Y":
                 system("clear")
                 break
@@ -49,8 +67,9 @@ def menu_start():
                 print("")
                 sys.exit("Thank you for attention. Goodbye!")
             else:
-                raise ValueError(Fore.RED + ("Please choose Y or N") +
-                    Style.RESET_ALL)
+                raise ValueError(
+                    Fore.RED + ("Please choose Y or N") + Style.RESET_ALL
+                )
         except ValueError as err:
             print(f"Invalid input: {err}")
 
