5632d7c5e7ef07d4826f5de618503066ca534d5c
Dymtro Chesnokov <dimmando@ukr.net>
2024-07-27 12:19:21 +0000

    Refactor print Intro function

--- run.py
+++ run.py
@@ -49,22 +49,23 @@ def introduction():
     """Introduction function"""
 
     print(
-        f"""{Fore.YELLOW}
-        Welcome to our program that helps to find out your risk of 
-                    type 2 diabetes in the next 10 years"""
-    )
+        f"{Fore.YELLOW}",
+        f"        Welcome to our program that helps to find out your risk of\n",
+        f"                  type 2 diabetes in the next 10 years")
+    print("")
     print(
-        f"""
-To calculate your risk we will ask you for some data related to your health.
-This program doesn't save or transfer any data you provided so anyone cannot 
-identify you. You are in safe place.{Style.RESET_ALL}"""
+        f"To calculate your risk we will ask you for some data related to your", 
+        f"health. This program doesn't save or transfer any data you provided", 
+        f"so anyone cannot identify you. You are in safe place.",
+        f"{Style.RESET_ALL}"
     )
+    print("")
     print(
-        f"""
-When you pass through our test you'll find out that potential risk level for 
-developing diabetes in the next 10 years of life depending on points earned:
-    """
+        f"When you pass through our test you'll find out that potential risk",
+        f"level for developing diabetes in the next 10 years of life depending",
+        f"on points earned:"
     )
+    print("")
     print(Fore.GREEN + "Less than 12 points" + Style.RESET_ALL + ": Low risk")
     print(
         Fore.CYAN + "12 - 14 points" + Style.RESET_ALL + ":      Average risk"
